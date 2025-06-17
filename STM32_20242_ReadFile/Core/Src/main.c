1934d7739fb2f7d65fefc1dfa46b89cb71442ec8
Doan Quyen <thuquyentntl@gmail.com>
2025-06-17 12:50:15 +0700

    thành công thao tác với file

--- STM32_20242_ReadFile/Core/Src/main.c
+++ STM32_20242_ReadFile/Core/Src/main.c
@@ -31,6 +31,7 @@
 
 /* Private typedef -----------------------------------------------------------*/
 /* USER CODE BEGIN PTD */
+//#define f_size(fp)  ((fp)->obj.objsize)
 
 /* USER CODE END PTD */
 
@@ -84,7 +85,8 @@ void send_uart(char *string)
 	memset(buf2, 0, sizeof(buf2));
 	HAL_UART_Transmit(&huart1, (uint8_t)* string, len, 2000);
 	*/
-  char buf1[100];
+	uint16_t len = strlen(string);
+  char buf1[len];
   memset(buf1, 0, sizeof(buf1));
   strcpy(buf1, string);
   HAL_UART_Transmit(&huart1,(char*) buf1, sizeof(buf1), 2000);
@@ -101,6 +103,12 @@ void bufclear(void){
 	for (int i=0;i<1024;i++){
 		buffer[i] = '\0';
 	}
+}
+
+
+void clear_buffer (void)
+{
+	for (int i=0; i<1024; i++) buffer[i] = '\0';
 }
 /* USER CODE END 0 */
 
@@ -141,7 +149,7 @@ int main(void)
   /* USER CODE BEGIN 2 */
   HAL_Delay(500);
   // Mount the SD card
-
+/*
   fresult = f_mount(&fs, "", 0);  // Changed to "0:" and force mount
   if (fresult != FR_OK) {
     switch(fresult) {
@@ -183,6 +191,7 @@ int main(void)
 
 /* Open file to write */
   // check free space
+  /*
   f_getfree("", &fre_clust, &pfs);
   total = (uint32_t)((pfs->n_fatent -2)* pfs->csize*0.5);
   sprintf(buffer, "SD Card total size: \t%lu\n", total);
@@ -217,10 +226,10 @@ int main(void)
 
 
  //----------Update a file-------------/
-  fresult = f_open(&fil, 'file1.txt', FA_OPEN_ALWAYS | FA_WRITE);
+  fresult = f_open(&fil, 'file1.txt', FA_OPEN_EXISTING | FA_READ | FA_WRITE);
 
   // move to the end
-  fresult = f_lseek(&fil, fil.buf); // xem lai de la f.size
+  fresult = f_lseek(&fil, fil.obj.objsize); // xem lai de la f.size
 
 
   // write to string to file
@@ -245,6 +254,141 @@ int main(void)
   if (fresult == FR_OK){
 	  send_uart("file1.txt removed sucessfully\n");
   }
+*/
+  fresult = f_mount(&fs, "/", 1);
+    	if (fresult != FR_OK) send_uart ("ERROR in mounting SD CARD...\n\n");
+    	else send_uart("SD CARD mounted successfully...\n\n");
+
+
+    	/*************** Card capacity details ********************/
+
+    	/* Check free space */
+    	f_getfree("", &fre_clust, &pfs);
+
+    	total = (uint32_t)((pfs->n_fatent - 2) * pfs->csize * 0.5);
+    	sprintf (buffer, "SD CARD Total Size: \t%lu\n",total);
+    	send_uart(buffer);
+    	clear_buffer();
+    	free_space = (uint32_t)(fre_clust * pfs->csize * 0.5);
+    	sprintf (buffer, "SD CARD Free Space: \t%lu\n\n",free_space);
+    	send_uart(buffer);
+    	clear_buffer();
+
+
+
+    	/************* The following operation is using PUTS and GETS *********************/
+
+    	/* Open file to write/ create a file if it doesn't exist */
+        fresult = f_open(&fil, "file1.txt", FA_OPEN_ALWAYS | FA_READ | FA_WRITE);
+
+    	/* Writing text */
+    	f_puts("This data is from the FILE1.txt. And it was written using ...f_puts... ", &fil);
+
+    	/* Close file */
+    	fresult = f_close(&fil);
+
+    	if (fresult == FR_OK)send_uart ("File1.txt created and the data is written \n");
+
+    	/* Open file to read */
+    	fresult = f_open(&fil, "file1.txt", FA_READ);
+
+    	/* Read string from the file */
+    	f_gets(buffer, f_size(&fil), &fil);
+
+    	send_uart("File1.txt is opened and it contains the data as shown below\n");
+    	send_uart(buffer);
+    	send_uart("\n\n");
+
+    	/* Close file */
+    	f_close(&fil);
+
+    	clear_buffer();
+
+
+
+
+    	/**************** The following operation is using f_write and f_read **************************/
+
+    	/* Create second file with read write access and open it */
+    	fresult = f_open(&fil, "file2.txt", FA_CREATE_ALWAYS | FA_WRITE);
+
+    	/* Writing text */
+    	strcpy (buffer, "This is File2.txt, written using f_write and said Hello World\n");
+
+    	fresult = f_write(&fil, buffer, bufsize(buffer), &bw);
+
+    	send_uart ("File2.txt created and data is written\n");
+
+    	/* Close file */
+    	f_close(&fil);
+
+
+
+    	// clearing buffer to show that result obtained is from the file
+    	clear_buffer();
+
+    	/* Open second file to read */
+    	fresult = f_open(&fil, "file2.txt", FA_READ);
+    	if (fresult == FR_OK)send_uart ("file2.txt is open and the data is shown below\n");
+
+    	/* Read data from the file
+    	 * Please see the function details for the arguments */
+    	f_read (&fil, buffer, f_size(&fil), &br);
+    	send_uart(buffer);
+    	send_uart("\n\n");
+
+    	/* Close file */
+    	f_close(&fil);
+
+    	clear_buffer();
+
+
+    	/*********************UPDATING an existing file ***************************/
+
+    	/* Open the file with write access */
+    	fresult = f_open(&fil, "file2.txt", FA_OPEN_EXISTING | FA_READ | FA_WRITE);
+
+    	/* Move to offset to the end of the file */
+    	fresult = f_lseek(&fil, f_size(&fil));
+
+    	if (fresult == FR_OK)send_uart ("About to update the file2.txt\n");
+
+    	/* write the string to the file */
+    	fresult = f_puts("This is updated data and it should be in the end", &fil);
+
+    	f_close (&fil);
+
+    	clear_buffer();
+
+    	/* Open to read the file */
+    	fresult = f_open (&fil, "file2.txt", FA_READ);
+
+    	/* Read string from the file */
+    	fresult = f_read (&fil, buffer, f_size(&fil), &br);
+    	if (fresult == FR_OK)send_uart ("Below is the data from updated file2.txt\n");
+    	send_uart(buffer);
+    	send_uart("\n\n");
+
+    	/* Close file */
+    	f_close(&fil);
+
+    	clear_buffer();
+
+    	/*
+
+    	/*************************REMOVING FILES FROM THE DIRECTORY ****************************/
+    	/*
+    	fresult = f_unlink("/file1.txt");
+    	if (fresult == FR_OK) send_uart("file1.txt removed successfully...\n");
+
+    	fresult = f_unlink("/file2.txt");
+    	if (fresult == FR_OK) send_uart("file2.txt removed successfully...\n");
+
+    	/* Unmount SDCARD */
+    	/*
+    	fresult = f_mount(NULL, "/", 1);
+    	if (fresult == FR_OK) send_uart ("SD CARD UNMOUNTED successfully...\n");
+    	*/
 
   /* USER CODE END 2 */
 
